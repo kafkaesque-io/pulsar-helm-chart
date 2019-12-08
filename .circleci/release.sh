007460a6a32d7c180638d58b2661e5529bb989ce
Chris Bartholomew <c_bartholomew@yahoo.com>
2019-12-07 19:42:49 -0500

    Release script fix

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -73,7 +73,7 @@ release_charts() {
 }
 
 update_index() {
-    chart-releaser index -o kafkaesque-io -r pulsar-helm-chart -p .deploy/index.yaml -t "$CH_TOKEN"
+    chart-releaser index -o kafkaesque-io -r pulsar-helm-chart -i .deploy/index.yaml -p .deploy -t "$CH_TOKEN" -c "https://helm.kafkaeque.io/"
 
     git config user.email "$GIT_EMAIL"
     git config user.name "$GIT_USERNAME"
