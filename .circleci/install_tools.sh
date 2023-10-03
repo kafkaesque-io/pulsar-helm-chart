dec3864e3e074fd020a359b9d87f5192b3cba927
Chris Bartholomew <c_bartholomew@yahoo.com>
2023-10-03 15:37:54 -0400

    Update install_tools.sh

--- .circleci/install_tools.sh
+++ .circleci/install_tools.sh
@@ -6,7 +6,7 @@ readonly HELM_VERSION=2.17.0
 readonly CHART_RELEASER_VERSION=0.2.3
 
 echo "Installing Helm..."
-curl -LO "https://kubernetes-helm.storage.googleapis.com/helm-v$HELM_VERSION-linux-amd64.tar.gz"
+curl -LO "https://get.helm.sh/helm-v$HELM_VERSION-linux-amd64.tar.gz"
 sudo mkdir -p "/usr/local/helm-v$HELM_VERSION"
 sudo tar -xzf "helm-v$HELM_VERSION-linux-amd64.tar.gz" -C "/usr/local/helm-v$HELM_VERSION"
 sudo ln -s "/usr/local/helm-v$HELM_VERSION/linux-amd64/helm" /usr/local/bin/helm
